087f0ce6b5a7f1310772cf5e94ac36cc118f6a18
John Fawcett <jfawcett@cloudflare.com>
2024-03-15 10:49:21 -0500

    Hi banda

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const router = Router();
 Our index route, a simple hello world.
 */
 router.get('/', () => {
-	return new Response('Hello, world! This is the root page of your Worker template.');
+	return new Response('Hi banda!');
 });
 
 /*
