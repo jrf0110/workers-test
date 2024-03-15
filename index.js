3c2a38865d0507c8fcace92e42d6620768fd9c3f
John Fawcett <jfawcett@cloudflare.com>
2024-03-15 10:51:20 -0500

    Revert to hello world

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const router = Router();
 Our index route, a simple hello world.
 */
 router.get('/', () => {
-	return new Response('Hi banda!');
+	return new Response('Hello, world! This is the root page of your Worker template.');
 });
 
 /*
